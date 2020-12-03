8bb820d13c7f1e4563fcce29a2ad8c046b6d5194
TomTomW90 <t.wiaderek@gmail.com>
2020-12-03 16:57:40 +0100

    Python challange Q17

--- Py_chelenge.py
+++ Py_chelenge.py
@@ -158,15 +158,194 @@ without,hello,bag,world
 Then, the output should be:
 bag,hello,without,world
 """
-values = str.split('5,10,8,1', ',')
-values_listed = [int(x) for x in values]
-sorted_values = sorted(values_listed)
-sv2 = [str(x) for x in sorted_values]
-sv3 = str.join(sv2)
 
+# values = str.split('without,hello,bag,world', ',')
+# values_listed = sorted(values)
+# values_sorted = ','.join(values_listed)
+#
+# items=[x for x in raw_input().split(',')]
+# items.sort()
+# print ','.join(items)
 
-print(values)
-print(values_listed)
-print(sorted_values)
-print(sv2)
-print(sv3)
+"""
+#----------------------------------------#
+Question 9
+Level 2
+
+Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized.
+Suppose the following input is supplied to the program:
+Hello world
+Practice makes perfect
+Then, the output should be:
+HELLO WORLD
+PRACTICE MAKES PERFECT
+"""
+
+# lines = []
+# while True:
+#     s = raw_input()
+#     if s:
+#         lines.append(s.upper())
+#     else:
+#         break;
+#
+# for sentence in lines:
+#     print sentence
+
+"""
+#----------------------------------------#
+Question 10
+Level 2
+
+Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing all duplicate words and sorting them alphanumerically.
+Suppose the following input is supplied to the program:
+hello world and practice makes perfect and hello world again
+Then, the output should be:
+again and hello makes perfect practice world
+"""
+
+# values = "hello world and practice makes perfect and hello world again"
+# values_list = [word for word in values.split()]
+# print(" ".join(sorted(set(values_list))))
+
+"""
+#----------------------------------------#
+Question 11
+Level 2
+
+Question:
+Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and then check whether they are divisible by 5 or not. The numbers that are divisible by 5 are to be printed in a comma separated sequence.
+Example:
+0100,0011,1010,1001
+Then the output should be:
+1010
+"""
+
+"""
+#----------------------------------------#
+Question 12
+Level 2
+
+Question:
+Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number is an even number.
+The numbers obtained should be printed in a comma-separated sequence on a single line.
+"""
+
+# values = [num for num in range(1000, 3001)]
+# values_new = [num for num in range(1000, 3001)]
+# for number in values:
+#     for digit in str(number):
+#         if int(digit) % 2 != 0:
+#             try: values_new.remove(int(number))
+#             except ValueError:
+#                 pass
+# values_new_as_str = [str(num) for num in values_new]
+# print(','.join(values_new_as_str))
+
+"""
+#----------------------------------------#
+Question 13
+Level 2
+
+Question:
+Write a program that accepts a sentence and calculate the number of letters and digits.
+Suppose the following input is supplied to the program:
+hello world! 123
+Then, the output should be:
+LETTERS 10
+DIGITS 3
+"""
+# sentence = "hello world! 123"
+# total_letters = 0
+# total_digits = 0
+#
+# for mark in sentence:
+#     if mark.isalpha():
+#         total_letters += 1
+#     elif mark.isdigit():
+#         total_digits += 1
+#
+# print("LETTERS: {}\nDIGITS: {}".format(total_letters, total_digits))
+
+"""
+#----------------------------------------#
+Question 14
+Level 2
+
+Question:
+Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+Suppose the following input is supplied to the program:
+Hello world!
+Then, the output should be:
+UPPER CASE 1
+LOWER CASE 9
+"""
+
+# sentence = "Hello world!"
+# d = {'UPPER CASE': 0,
+#      'LOWER CASE': 0}
+#
+# for mark in sentence:
+#     if mark.isupper():
+#         d['UPPER CASE'] += 1
+#     elif mark.islower():
+#         d['LOWER CASE'] += 1
+#     else:
+#         pass
+#
+# print("Upper case: {}\nLower case: {}".format(d['UPPER CASE'], d['LOWER CASE']))
+
+"""
+#----------------------------------------#
+Question 15
+Level 2
+
+Question:
+Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+Suppose the following input is supplied to the program:
+9
+Then, the output should be:
+11106
+"""
+# d = 9
+#
+# def compute(digit):
+#     total = digit + digit*11 + digit*111 + digit*1111
+#     return total
+#
+# print(compute(d))
+
+"""
+#----------------------------------------#
+Question 16
+Level 2
+
+Question:
+Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers.
+Suppose the following input is supplied to the program:
+1,2,3,4,5,6,7,8,9
+Then, the output should be:
+1,3,5,7,9
+"""
+# sequance = "1,2,3,4,5,6,7,8,9"
+# values = [int(num)**2 for num in sequance.split(',') if int(num) % 2 != 0]
+
+"""
+#----------------------------------------#
+Question 17
+Level 2
+
+Question:
+Write a program that computes the net amount of a bank account based a transaction log from console input. The transaction log format is shown as following:
+D 100
+W 200
+
+D means deposit while W means withdrawal.
+Suppose the following input is supplied to the program:
+D 300
+D 300
+W 200
+D 100
+Then, the output should be:
+500
+"""
